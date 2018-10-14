187f0ab054dd047d9637435372475a4313376486
joooo <zz_o@hanmail.net>
2018-10-14 22:03:03 +0900

    10월 14일 PP07.TextureManager

---
+++ PP08.SingletonTM/main.cpp
@@ -0,0 +1,22 @@
+#include "Game.h"
+#include "TextureManager.h"
+#include <iostream>
+using namespace std;
+
+Game* g_game = 0;
+
+int main(int argc, char* argv[])
+{
+	g_game = new Game();
+	g_game->init("Chapter 7", 100, 100, 640, 480, false);
+
+	while (g_game->running())
+	{
+		g_game->handleEvents();
+		g_game->update();
+		g_game->render();
+	}
+	SDL_Delay(5000);
+	g_game->clean();
+	return 0;
+}
