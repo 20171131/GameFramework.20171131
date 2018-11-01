940cf71127a94c9ca95a07340cf76f00c79c3f5f
ohgyukwon <20171106@vision.hoseo.edu>
2018-11-01 18:07:21 +0900

    동방가서 마저 하기

---
+++ PP14.MInputHandler/Enemy.cpp
@@ -0,0 +1,31 @@
+#include "Enemy.h"
+#include "InputHandler.h"
+
+Enemy::Enemy(const LoaderParams* pParams) : SDLGameObject(pParams)
+{
+
+}
+
+void Enemy::draw()
+{
+	SDLGameObject::draw(); // we now use SDLGameObject
+}
+
+void Enemy::update()
+{
+	m_position.setX(m_position.getX() + 1);
+	m_position.setY(m_position.getY() + 1);
+
+	m_currentFrame = int(((SDL_GetTicks() / 100) % 6));
+}
+
+void Enemy::clean()
+{
+
+}
+
+void Enemy::handleInput()
+{
+	Vector2D* vec = TheInputHandler::Instance()->getMousePosition();
+	m_velocity = (*vec - m_position) / 100;
+}
