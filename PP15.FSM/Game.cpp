5d1eb60c3ac758b4accd6998418825a1f7e18626
kachu9875 <20171132@vision.hoseo.edu>
2018-11-20 13:17:34 +0900

    15마저 하기

---
+++ PP15.FSM/Game.cpp
@@ -0,0 +1,65 @@
+#include "Game.h"
+#include "MenuState.h"
+#include <SDL_image.h>
+#include <iostream>
+
+Game* Game::s_pInstance = 0;
+
+bool Game::init(const char* title, int xpos, int ypos, int width, int height, bool fullscreen)
+{
+	if (SDL_Init(SDL_INIT_EVERYTHING) >= 0)
+	{
+		m_pWindow = SDL_CreateWindow(title, xpos, ypos, width, height, SDL_WINDOW_SHOWN);
+
+		if (m_pWindow != 0)
+		{
+			m_pRenderer = SDL_CreateRenderer(m_pWindow, -1, 0);
+		}
+
+		m_bRunning = true;
+
+		m_pGameStateMachine = new GameStateMachine();
+		m_pGameStateMachine->changeState(MenuState::Instance());
+
+	}
+	else 
+	{
+		return false; // sdl could not initialize
+	}
+	return true;
+}
+
+void Game::render()
+{
+	SDL_RenderClear(m_pRenderer);
+	m_pGameStateMachine->render();
+	SDL_RenderPresent(m_pRenderer);
+}
+
+void Game::clean()
+{
+	std::cout << "cleaning game" << std::endl;
+	SDL_DestroyWindow(m_pWindow);
+	SDL_DestroyRenderer(m_pRenderer);
+	SDL_Quit();
+}
+
+void Game::handleEvents()
+{
+	TheInputHandler::Instance()->update();
+	if (TheInputHandler::Instance()->isKeyDown(SDL_SCANCODE_RETURN))
+	{
+		m_pGameStateMachine->changeState(PlayState::Instance());
+	}
+
+}
+
+void Game::update()
+{
+	m_pGameStateMachine->update();
+}
+
+void Game::quit()
+{
+
+}
